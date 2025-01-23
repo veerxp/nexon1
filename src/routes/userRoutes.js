9eb2217f0c6bdc3ba55ebd6a74bec59986eb9d27
veerxp <virbhadra2406@gmail.com>
2025-01-23 08:20:23 +0530

    users

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const verifyToken = require("../middleware/authMiddleware");
+const authorizeRoles = require("../middleware/roleMiddleware");
 const router = express.Router();
 
 // only admin can access the router
-router.get("/admin", verifyToken, (req,res) => {
+router.get("/admin", verifyToken, authorizeRoles("admin"), (req,res) => {
     res.json({ message : "welcome admin"});
 });
 // only maanger can access the router
-router.get("/manager", (req,res) => {
+router.get("/manager", verifyToken, authorizeRoles("admin","manager"), (req,res) => {
     res.json({ message : "welcome manager"});
 });
 
-
-router.get("/employee", (req,res) => {
+//All can acceess this
+router.get("/employee", verifyToken, authorizeRoles("admin","manager","employee"), (req,res) => {
     res.json({ message : "welcome employee"});
 });
 
